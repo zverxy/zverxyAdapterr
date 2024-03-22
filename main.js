b18bba849ab42af3eed8ee08c4f01752a2defe6a
zverxy <112375816+zverxy@users.noreply.github.com>
2024-03-23 01:02:03 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$(window).on('load pageshow', function () {
+    $('body').fadeIn();
+});    
+$("a:not([href*=javascript]):not([href*=\\#]):not(.fancybox):not([target]):not([data-fancybox])").click(function() {
+    $('body').fadeOut();
+    let url = $(this).attr('href');
+    window.setTimeout(function() {
+        window.location.href = url;
+    }, 500);    
+    return  true;
+    
+});
